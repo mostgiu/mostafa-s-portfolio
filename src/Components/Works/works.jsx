35ea7155eb9efa47c6ec88b3e1dc8f5aac71389b
mostgiu <130293877+mostgiu@users.noreply.github.com>
2026-01-22 22:00:03 +0100

    intro text improvments

--- src/Components/Works/works.jsx
+++ src/Components/Works/works.jsx
@@ -3,13 +3,13 @@ import "./works.css";
 import Uni from "../../assets/uni.jpg";
 import To_do from "../../assets/to-do.jpg";
 import Aos from "aos";
-import 'aos/dist/aos.css'
-import { useEffect } from 'react';
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Works = () => {
-    useEffect(()=>{
-         Aos.init({duration:1000})
-       },[])
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
   return (
     <section id="works">
       <h2 className="worksTitle">My Portfolio</h2>
@@ -20,21 +20,32 @@ const Works = () => {
         online presence.
       </span>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div data-aos='fade-right'  className="project">
-          <img src={Uni} alt="" className="worksImg" />
-          <div className="layer">
-            <a
-              href="https://www.w3schools.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-            >
-              <span>University website </span><br />
-             <div className="text-xs">View Project <i className="fa-solid fa-arrow-right"></i></div> 
-            </a>
+        <div
+          data-aos="fade-right"
+          className="project bg-gray-100 block max-w-sm  border border-gray-300 shadow-md p-4"
+        >
+          <div className="img-container">
+            <img src={Uni} alt="" className="worksImg" />
+          </div>
+          <div className="card-title text-start">
+            <div className="card-header flex gap-2 items-center mb-2">
+              <h2 className="font-bold text-2xl ">University Website</h2>
+            </div>
+            <div className="card-desc ">
+             
+              <p className="text-gray-600 text-sm ">
+                A fully responsive university website showcasing structured
+                content, clean UI design, and front-end best practices
+                
+              </p>
+              <div className="project-link hover:text-black text-stone-800 border border-black border-3 rounded-2xl flex w-35 h-10 justify-center items-center"> <a href="">
+                  View Project <i className="fa-solid fa-up-right-from-square"></i>
+                </a></div>
+             
+            </div>
           </div>
         </div>
-        <div data-aos='fade-left'  className="project">
+        <div data-aos="fade-left" className="project max-w-sm ">
           <img src={To_do} alt="" className="worksImg" />
           <div className="layer">
             <a
@@ -43,16 +54,39 @@ const Works = () => {
               rel="noopener noreferrer"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
             >
-              <span> To Do App </span><br />
-             <div className="text-xs">View Project <i className="fa-solid fa-arrow-right"></i></div> 
+              <span> To Do App </span>
+              <br />
+              <div className="text-xs">
+                View Project <i className="fa-solid fa-arrow-right"></i>
+              </div>
             </a>
           </div>
+          Manage your tasks effortlessly with this React app—add, update, and
+          delete items in a clean, responsive interface.
         </div>
-       
+        <div className="bg-gray-100 block max-w-sm p-6 border border-gray-300 rounded-lg shadow-md">
+          <a href="#">
+            <img className="rounded-lg" src={Uni} alt="Blog Image" />
+          </a>
+          <a href="#">
+            <h5 className="mt-6 mb-2 text-2xl font-semibold tracking-tight text-gray-800">
+              Streamlining your design process today.
+            </h5>
+          </a>
+          <p className="mb-6 text-gray-600">
+            In today’s fast-paced digital landscape, fostering seamless
+            collaboration among Developers and IT Operations.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center text-white   font-medium rounded-lg text-sm px-4 py-2 shadow focus:outline-none focus:ring-2 focus:ring-blue-400"
+          >
+            Read
+          </a>
+        </div>
       </div>
     </section>
   );
 };
 
 export default Works;
-
